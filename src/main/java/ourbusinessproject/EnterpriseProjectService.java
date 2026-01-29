b5634b40d9bba8570bb3f4237d70e99362af849d
ClementReverbel <reverbel.cle@gmail.com>
2026-01-29 08:50:38 +0100

    fix #3.5 optimization SQL

--- src/main/java/ourbusinessproject/EnterpriseProjectService.java
+++ src/main/java/ourbusinessproject/EnterpriseProjectService.java
@@ -58,7 +58,7 @@ public class EnterpriseProjectService {
     }
 
     public List<Project> findAllProjects(){
-        String query = "SELECT p FROM Project p ORDER BY p.title";
+        String query = "SELECT p FROM Project p JOIN FETCH p.enterprise ORDER BY p.title";
         TypedQuery<Project> queryObj = EM.createQuery(query, Project.class);
         return queryObj.getResultList();
     }
